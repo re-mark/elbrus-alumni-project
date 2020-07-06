eb9814507f7c098a4db2beca426210935aaacdcf
re-mark <a_hl@mail.ru>
2020-07-06 23:24:17 +1000

    fixes

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const app = express();
 
 // Подключаем mongoose.
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/user_elbrus', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/user_elbrus', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,21 +32,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/register', usersRouter);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(3002);
-
+app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    surname: String,
-    nickname: String,
-    email: String,
-    password: String,
-    phone: String,
-    location: String,
-    Location_chenge: Boolean,
-    job: Boolean,
-    gitHub_link: String,
-    linkedin_link: String,
-    abou_user: String,
-    projects: Array,
-    skils: Array,
-    admin: Boolean,
-    admin_phrase: String
-  });
-
-
+  name: String,
+  surname: String,
+  nickname: String,
+  email: String,
+  password: String,
+  phone: String,
+  location: String,
+  Location_change: Boolean,
+  job: Boolean,
+  gitHub_link: String,
+  linkedin_link: String,
+  about_user: String,
+  projects: Array,
+  skills: Array,
+  admin: Boolean,
+  admin_phrase: String,
+});
 
 module.exports = mongoose.model('User', userSchema);
