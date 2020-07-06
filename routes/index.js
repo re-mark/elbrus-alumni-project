8909ce488c1de6a878629b101d45866431c9bd12
anton-iavorskii <89117058000@mail.ru>
2020-07-06 16:24:23 +0300

    Anton

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
